9d6e534d0f7b8c27dcfbf9073026eed8c5aca29f
Anikendra Datta <anikdatta7553@gmail.com>
2020-07-30 20:42:17 +0530

    Updates

---
+++ precache-manifest.7d81931f237e9819696dde309ed5243a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ac07504fc81f823039f288b414bc83",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c90ab18ed9122c88c2a4",
+    "url": "/weather-app/static/css/main.3fcbb9e1.chunk.css"
+  },
+  {
+    "revision": "d04dc7a823a1c0813acf",
+    "url": "/weather-app/static/js/2.5c94b2fc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/weather-app/static/js/2.5c94b2fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c90ab18ed9122c88c2a4",
+    "url": "/weather-app/static/js/main.fcb98d70.chunk.js"
+  },
+  {
+    "revision": "06bb25e49de1386edcac",
+    "url": "/weather-app/static/js/runtime-main.7fed549c.js"
+  }
+]);
